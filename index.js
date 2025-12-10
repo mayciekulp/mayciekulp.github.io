91585567d48b4204a2897b86d4a4eff84b4f623a
mayciekulp <maycie.kulp@gmail.com>
2025-12-09 20:47:45 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function goToHistoryWrong() {
+    window.location.href = 'wrongAnswer.html';
+}
+
+
+function goToHistoryRight() {
+    window.location.href = 'rightAnswer.html';
+}
+
+
+function goToIndex() {
+    window.location.href = 'index.html';
+}
